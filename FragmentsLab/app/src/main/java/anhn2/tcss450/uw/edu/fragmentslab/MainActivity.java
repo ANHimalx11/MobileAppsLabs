57795d9f2b504298723a676eca71456d247ef2d6
Anh Nguyen <anhnguyen526@gmail.com>
2018-04-10 01:43:55 -0700

    lab2 start commit

---
+++ FragmentsLab/app/src/main/java/anhn2/tcss450/uw/edu/fragmentslab/MainActivity.java
@@ -0,0 +1,13 @@
+package anhn2.tcss450.uw.edu.fragmentslab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
